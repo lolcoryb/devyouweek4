2a5ebc32c943c6c863e83b3c0f17fc1dc893c929
lolcoryb <71185428+lolcoryb@users.noreply.github.com>
2021-05-09 22:09:13 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,183 @@
+﻿using System;
+
+namespace week4homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Use a while loop to print all whole numbers from 1 to n
+
+            //Console.WriteLine("Pick a number");
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //int a = 1;
+
+            //while (a <= num)
+
+            //{
+            //    Console.WriteLine(a);
+            //    a++;
+            //}
+
+            //Use a while loop to print all whole numbers in reverse from n to 1
+
+            //Console.WriteLine("Pick a number");
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //int a = 1;
+
+            //while (a <= num)
+
+            //{
+            //    Console.WriteLine(num);
+            //    num--;
+            //}
+
+
+            //Use a while loop to print all the alphabets from a - z
+            //Console.WriteLine("Let's read the Alphabet!");
+            //string abc = ("a");
+            //while (abc == "a") 
+            //{
+            //    Console.WriteLine("a\r\nb\r\nc\r\nd\r\ne\r\nf\r\ng\r\nh\r\ni\r\nj\r\nk\r\nl\r\nm\r\nn\r\no\r\np\r\nq\r\nr\r\ns\r\nt\r\nu\r\nv\r\nw\r\nx\r\ny\r\nz");
+            //    Console.ReadLine();
+            //}
+
+            //Use a while loop to print all even numbers between 1 and 100
+            //why wont this print the number 100 in the results?
+            //Console.WriteLine("Here's all the even numbers between 1 and 100");
+
+            //int num = 100;
+
+            //while (num <= 100 && num >=0)
+            //{
+
+            //    num--;
+
+            //    if (num % 2 == 0)
+
+            //        Console.WriteLine(num);
+            //}
+
+            //Use a while loop to print all odd numbers between 1 and 100
+
+            //Console.WriteLine("Heres all of the odd numbers between 1 and 100");
+
+            //int num = 100;
+
+            //    while (num <= 100 && num >0)
+            //{
+            //    num--;
+            //    if (num % 2 != 0)
+            //        Console.WriteLine(num);
+
+
+            //Use a while loop to print the sum of all the whole numbers from 1 - n
+
+            //Console.WriteLine("Pick a number");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //int z = 0;
+
+            //while (n >= 1)
+            //{
+            //    z = z + n;
+            //        n--;
+
+            //}
+
+            //if (n == 0)
+            //{
+            //    Console.WriteLine($"{z}");
+            //}
+
+            //Given the number n, use a while loop to print the multiplication table up to n.
+
+            //Console.WriteLine("Pick a number");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //int n1 = 0;
+            //while (n1<= n - 1)
+            //{
+            //    n1++;
+            //    int r = n * n1;
+            //    Console.WriteLine($"{n} * {n1} = {r}");
+            //}
+
+        }
+    }
+    }
+
+
+            
+
+
+
+
+                //Problems all working! Comment/uncomment sections of code to see!  I have commented out all but one so the console was clearer to read. 
+                //The letter ‘n’ represents user input.
+                //Use a while loop to print all whole numbers from 1 to n
+                //Console.WriteLine("Enter a number");
+                //int n = int.Parse(Console.ReadLine());
+                //int i = 1;
+                //while (i<=n) {
+                //    Console.WriteLine(i);
+                //    i++;
+                //}
+                //
+                //Use a while loop to print all whole numbers in reverse from n to 1
+                //while (i<=n)
+                //{
+                //    Console.WriteLine(n);
+                //    n--;
+                //}
+                //
+                //Use a while loop to print all the alphabets from a - z
+                //int i = -1;
+                //while (i < 25)
+                //{
+                //    i++;
+                //    Console.Write(Convert.ToChar(i + (int)'a') + " , ");
+                //}
+
+                //Use a while loop to print all even numbers between 1 and 100
+                //int i = 1;
+
+
+                //while (i >= 1 && i <= 100)
+                //{
+                //    i++;
+
+                //    if (i % 2 == 0)
+
+                //        Console.WriteLine(i);
+                //}
+                //Use a while loop to print all odd numbers between 1 and 100
+                //
+                //**bug : list returning one more than expected.
+                //int i = 0;
+                //while (i >= 0 && i <= 100)
+                //{
+                //    i++;
+
+                //    if (i % 2 == 1)
+
+                //        Console.WriteLine(i);
+                //}
+                //Use a while loop to print the sum of all the whole numbers from 1 - n
+                //Given the number n, use a while loop to print the multiplication table up to n.
+                //Example: n = 5
+                //5 * 1 = 5
+                //5 * 2 = 10
+                //5 * 3 = 15
+                //5 * 4 = 20
+                //5 * 5 = 25
+                //Console.WriteLine("Enter a number");
+                //int n = int.Parse(Console.ReadLine());
+                //int i = 0;
+                //while (i <= n - 1)
+                //{
+                    //i++;
+                    //int iProd = n * i;
+                    //Console.WriteLine(iProd);
+                //};
+
+            
+  
